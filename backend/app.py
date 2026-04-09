096004d05a3e444cab0f9c446427f9e49d3feeee
VisakanM-bit <visakan2005smr@gmail.com>
2026-04-09 10:01:48 +0530

    Fix vercel config filename

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 import os
-from backend.app import app
+
 load_dotenv()
 
-app = Flask(__name__,
+app = Flask(
+    __name__,
     template_folder="../templates",
     static_folder="../static"
 )
@@ -15,17 +16,23 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "success",
+        "message": "Backend working"
+    })
+
+
 @app.route('/api/history-data')
 def history_data():
     return jsonify({
-        "timestamps": [
-            "10:00", "10:05", "10:10", "10:15", "10:20"
-        ],
-        "temperature": [30, 32, 34, 33, 35],
-        "humidity": [45, 46, 48, 47, 49],
-        "static_charge": [50, 52, 57, 55, 58],
-        "voltage": [220, 221, 219, 222, 223],
-        "risk_score": [0.2, 0.4, 0.8, 0.6, 0.9]
+        "timestamps": ["10:00","10:05","10:10","10:15","10:20"],
+        "temperature": [30,32,34,33,35],
+        "humidity": [45,46,48,47,49],
+        "static_charge": [50,52,57,55,58],
+        "voltage": [220,221,219,222,223],
+        "risk_score": [0.2,0.4,0.8,0.6,0.9]
     })
 
 
